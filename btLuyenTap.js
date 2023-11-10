93b87899e8898b92ad87d85310588b41170b6efa
tuananhpaddy <tuananhtran.xd@gmail.com>
2023-11-10 14:45:59 +0700

    them bai tap

---
+++ btLuyenTap.js
@@ -0,0 +1,6 @@
+function traloi() {
+    let tl= prompt("nhập câu trả lời");
+    if (tl == "ECMAScript") {
+        alert("Đúng rồi đấy");
+    } else alert("Thế mà cũng không biết. Là ECMAScript");
+}
